449a2ec49ffb567ac27009bace44906481b41f8b
Arthur Dos Santos Almeida <arthur.almeida@g.globo>
2022-09-29 14:29:00 -0300

    feat: implementando plugin paradizo

--- cotuba/src/main/java/cotuba/md/RenderizadorMDParaHTML.java
+++ cotuba/src/main/java/cotuba/md/RenderizadorMDParaHTML.java
@@ -1,6 +1,7 @@
 package cotuba.md;
 
 import cotuba.domain.Capitulo;
+import cotuba.plugin.Plugin;
 import org.commonmark.node.*;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
@@ -54,6 +55,7 @@ public class RenderizadorMDParaHTML {
             HtmlRenderer renderer = HtmlRenderer.builder().build();
             String html = renderer.render(document);
             capitulo.setConteudoHTML(html);
+            Plugin.renderizou(capitulo);
         } catch (Exception ex) {
             throw new IllegalStateException("Erro ao renderizar para HTML o arquivo " + arquivoMD, ex);
         }
---
+++ cotuba/src/main/java/cotuba/plugin/Plugin.java
@@ -0,0 +1,27 @@
+package cotuba.plugin;
+
+import cotuba.domain.Capitulo;
+import cotuba.domain.Ebook;
+
+import java.util.ServiceLoader;
+
+//SERVICE PROVIDER INTERFACE
+public interface Plugin {
+    String aposRenderizacao(String html);
+
+    void aposGeracao(Ebook ebook);
+
+    static void renderizou(Capitulo capitulo){
+        ServiceLoader.load(Plugin.class).forEach(plugin -> {
+            String html = capitulo.getConteudoHTML();
+            String htmlModificado = plugin.aposRenderizacao(html);
+            capitulo.setConteudoHTML(htmlModificado);
+        });
+    }
+
+    static void gerou(Ebook ebook){
+        ServiceLoader.load(Plugin.class).forEach(plugin -> {
+            plugin.aposGeracao(ebook);
+        });
+    }
+}
