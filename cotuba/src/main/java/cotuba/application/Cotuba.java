26f64f5d82329c595057703910b25c19e1c66a2e
Arthur Dos Santos Almeida <arthur.almeida@g.globo>
2022-09-30 16:12:42 -0300

    feat: implementando principio de substituicao de liskov

--- cotuba/src/main/java/cotuba/application/Cotuba.java
+++ cotuba/src/main/java/cotuba/application/Cotuba.java
@@ -4,7 +4,7 @@ import cotuba.domain.Capitulo;
 import cotuba.domain.Ebook;
 import cotuba.domain.FormatoEbook;
 import cotuba.md.RenderizadorMDParaHTML;
-import cotuba.plugin.Plugin;
+import cotuba.plugin.AoFinalizarGeracao;
 
 import java.nio.file.Path;
 import java.util.List;
@@ -33,6 +33,6 @@ public class Cotuba {
 
         GeradorEbook geradorEbook = GeradorEbook.cria(formato);
         geradorEbook.gera(ebook);
-        Plugin.gerou(ebook);
+        AoFinalizarGeracao.gerou(ebook);
     }
 }
--- cotuba/src/main/java/cotuba/plugin/AoRenderizarHTML.java
+++ cotuba/src/main/java/cotuba/plugin/AoRenderizarHTML.java
@@ -1,27 +1,17 @@
 package cotuba.plugin;
 
 import cotuba.domain.Capitulo;
-import cotuba.domain.Ebook;
-
 import java.util.ServiceLoader;
 
 //SERVICE PROVIDER INTERFACE
-public interface Plugin {
+public interface AoRenderizarHTML {
     String aposRenderizacao(String html);
 
-    void aposGeracao(Ebook ebook);
-
     static void renderizou(Capitulo capitulo){
-        ServiceLoader.load(Plugin.class).forEach(plugin -> {
+        ServiceLoader.load(AoRenderizarHTML.class).forEach(plugin -> {
             String html = capitulo.getConteudoHTML();
             String htmlModificado = plugin.aposRenderizacao(html);
             capitulo.setConteudoHTML(htmlModificado);
-        });
-    }
-
-    static void gerou(Ebook ebook){
-        ServiceLoader.load(Plugin.class).forEach(plugin -> {
-            plugin.aposGeracao(ebook);
         });
     }
 }
