4bb95307e88ddf6bc24da82e7242e816cea44653
Arthur Dos Santos Almeida <arthur.almeida@g.globo>
2022-11-01 15:47:24 -0300

    feat: criando teste unitario para validar args passados na cli

--- cotuba/src/test/java/cotuba/ports/unit/CotubaCliTest.java
+++ cotuba/src/test/java/cotuba/ports/unit/CotubaCliTest.java
@@ -1,66 +1,29 @@
-package cotuba.ports.integration;
+package cotuba.ports.unit;
 
-import cotuba.application.Cotuba;
-import cotuba.application.ParametrosCotuba;
 import cotuba.cli.LeitorOpcoesCLI;
 import cotuba.cli.Main;
 import cotuba.domain.FormatoEbook;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
 
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 public class CotubaCliTest {
-    //chamar o main, passando os args, n deixa chamar o cotuba , conferir se os dados foram passados corretamente
 
     @Test
-    public void deveReceberArgumentosEChamarCotubaPassandoParametrosCorretamente(){
+    public void deveMontarParametrosComArgumentosRecebidosCorretamente(){
         String[] argumentos = new String[4];
         argumentos[0] = "-d";
         argumentos[1] = "./livro-exemplo";
         argumentos[2] = "-f";
         argumentos[3] = "epub";
 
-        Cotuba cotuba = mock(Cotuba.class);
-        //ArgumentCaptor<ParametrosCotuba> valorCapturado = ArgumentCaptor.forClass(ParametrosCotuba.class);
-        //doNothing().when(cotuba).executa(valorCapturado.capture());
+        LeitorOpcoesCLI parametrosCotuba = Main.montaParametrosComArgumentosRecebidos(argumentos);
 
-        ParametrosCotuba parametrosCotuba = new LeitorOpcoesCLI(argumentos);
-        //doCallRealMethod().when(cotuba).executa(parametrosCotuba);
-
-        Mockito.doCallRealMethod().when(cotuba).executa(parametrosCotuba);
-        Main.main(argumentos);
-
-        //doAnswer(new Answer() {
-            //@Override
-            //public Object answer(InvocationOnMock invocationOnMock) throws Throwable {
-            //    System.out.println("::::::");
-            //    Main.main(argumentos);
-            //    return null;
-            //}
-        //}).when(cotuba).executa(parametrosCotuba);
-
-        //System.out.println(valorCapturado.capture());
-
-        //assertEquals(FormatoEbook.EPUB, valorCapturado.capture().getFormato());
-        //assertEquals(Paths.get("./livro-exemplo"), valorCapturado.capture().getDiretorioDosMD());
-        //assertEquals(Paths.get("book.epub"), valorCapturado.capture().getArquivoDeSaida());
-
-
-        //doAnswer((i) -> {
-        //    assertEquals(FormatoEbook.EPUB, valorCapturado.capture().getFormato());
-        //    assertEquals(Paths.get("./livro-exemplo"), valorCapturado.capture().getDiretorioDosMD());
-        //    assertEquals(Paths.get("book.epub"), valorCapturado.capture().getArquivoDeSaida());
-        //    return null;
-        //}).when(cotuba).executa(any());
-
-
+        assertEquals(FormatoEbook.EPUB, parametrosCotuba.getFormato());
+        assertEquals(Paths.get("book.epub"), parametrosCotuba.getArquivoDeSaida());
+        assertEquals(Paths.get("./livro-exemplo"), parametrosCotuba.getDiretorioDosMD());
     }
 
     @Test
@@ -68,33 +31,5 @@ public class CotubaCliTest {
         assertThrows(IllegalStateException.class, () -> {
             Main.main(null);
         });
-    }
-
-    @Test
-    void test_mockito_void() {
-        Employee emp = mock(Employee.class);
-
-        doAnswer((i) -> {
-            System.out.println(i);
-            System.out.println("Employee setName Argument = " + i.getArgument(0));
-            assertEquals("Pankaj", i.getArgument(0));
-            return null;
-        }).doCallRealMethod().when(emp).setName(anyString());
-
-        emp.setName("Pankaj");
-        assertEquals("Pankaj", emp.getName());
-    }
-
-    private class Employee {
-        private String name;
-
-        public void setName(String name) {
-            System.out.println("nem passou");
-            this.name = name;
-        }
-
-        public String getName() {
-            return name;
-        }
     }
 }
